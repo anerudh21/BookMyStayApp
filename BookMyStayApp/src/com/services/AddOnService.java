bb85a1a1a8c397cfd780c4e43d621ad8d1687c8b
anerudh21 <anerudh21@gmail.com>
2026-03-10 12:17:39 +0530

    UC-6 implemented booking history and reporting

---
+++ BookMyStayApp/src/com/services/AddOnService.java
@@ -0,0 +1,13 @@
+package com.services;
+
+public class AddOnService {
+    private String name;
+    private double price;
+
+    public AddOnService(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+}
